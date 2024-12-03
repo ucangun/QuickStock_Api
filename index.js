02b7d0a68638dd51d9e602ce7fd936e91c0c7186
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-03 12:02:39 +0100

    "Added express-async-errors package, updated index.js to use it, and modified server startup logging to use HOST environment variable"

--- index.js
+++ index.js
@@ -6,22 +6,31 @@
 
 const express = require("express");
 const app = express();
-const connectDB = require("./src/configs/dbConnection");
 
 /* ------------------------------------------------- */
 
 // envVariables to process.env:
 require("dotenv").config();
+const HOST = process.env?.HOST || "127.0.0.1";
 const PORT = process.env?.PORT || 8000;
+
+// asyncErrors to errorHandler:
+require("express-async-errors");
 
 /* ------------------------------------------------- */
 
 // DB Connection
+const connectDB = require("./src/configs/dbConnection");
 connectDB();
+
+/* ------------------------------------------------- */
+
+// errorHandler:
+app.use(require("./src/middlewares/errorHandler"));
 
 /* ------------------------------------------------- */
 
 // RUN SERVER:
 app.listen(PORT, () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`);
+  console.log(`Server running on http:${HOST}:${PORT}`);
 });
