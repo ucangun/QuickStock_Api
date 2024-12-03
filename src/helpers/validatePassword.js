758ee6f54938173ecfb50ffc5e4dec87fca92168
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-03 14:26:08 +0100

    "Created a new `userSchema` with strong password validation using `validatePassword` helper"

---
+++ src/helpers/validatePassword.js
@@ -0,0 +1,16 @@
+/* ------------------------------------------------- */
+/*                  QUICKSTOCK API                   */
+/* ------------------------------------------------- */
+
+const validator = require("validator");
+
+const validatePassword = (password) =>
+  validator.isStrongPassword(password, {
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  });
+
+module.exports = validatePassword;
---
+++ src/models/user.js
@@ -0,0 +1,82 @@
+"use strict";
+
+/* ------------------------------------------------- */
+/*                  QUICKSTOCK API                   */
+/* ------------------------------------------------- */
+
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const validatePassword = require("../helpers/validatePassword");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "Username is required"],
+      unique: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: [true, "Password is required"],
+      validate: {
+        validator: validatePassword,
+        message:
+          "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, one number, and one special character.",
+      },
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Email is required"],
+      unique: true,
+      index: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
+    },
+
+    firstName: {
+      type: String,
+      trim: true,
+      required: [true, "First name is required"],
+    },
+
+    lastName: {
+      type: String,
+      trim: true,
+      required: [true, "Last name is required"],
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { collection: "users", timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
