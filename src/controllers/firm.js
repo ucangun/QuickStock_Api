e58dd3ac98e0d83476c3fbc220a24a427f683bdd
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-04 16:36:36 +0100

    "Add Firm Controller"

--- src/controllers/firm.js
+++ src/controllers/firm.js
@@ -4,15 +4,15 @@
 /*                  QUICKSTOCK API                   */
 /* ------------------------------------------------- */
 
-// Category Controller
+// Firm Controller
 
-const Category = require("../models/category");
+const Firm = require("../models/firm");
 
 module.exports = {
   list: async (req, res) => {
     /*    
-            #swagger.tags = ["Categories"]
-            #swagger.summary = "List Categories"
+            #swagger.tags = ["Firms"]
+            #swagger.summary = "List Firms"
             #swagger.description = `
                 You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
                 <ul> Examples:
@@ -24,29 +24,29 @@ module.exports = {
             `
         */
 
-    const data = await res.getModelList(Category);
+    const data = await res.getModelList(Firm);
 
     res.status(200).send({
       error: false,
-      details: await res.getModelListDetails(Category),
+      details: await res.getModelListDetails(Firm),
       data,
     });
   },
 
   create: async (req, res) => {
     /*
-            #swagger.tags = ["Categories"]
-            #swagger.summary = "Create Category"
+            #swagger.tags = ["Firms"]
+            #swagger.summary = "Create Firm"
             #swagger.parameters['body'] = {
                 in: 'body',
                 required: true,
                 schema: {
-                    "name": "Category 1"
+                    "name": "Firm 1"
                 }
             }
         */
 
-    const data = await Category.create(req.body);
+    const data = await Firm.create(req.body);
 
     res.status(200).send({
       error: false,
@@ -56,8 +56,8 @@ module.exports = {
 
   read: async (req, res) => {
     /*
-            #swagger.tags = ["Categories"]
-            #swagger.summary = "Get Single Category"
+            #swagger.tags = ["Firms"]
+            #swagger.summary = "Get Single Firm"
         */
 
     if (req.params.id) {
@@ -65,15 +65,15 @@ module.exports = {
 
       res.status(200).send({
         error: false,
-        details: await res.getModelListDetails(Category),
+        details: await res.getModelListDetails(Firm),
         data,
       });
     } else {
-      const data = await res.getModelList(Category);
+      const data = await res.getModelList(Firm);
 
       res.status(200).send({
         error: false,
-        details: await res.getModelListDetails(Category),
+        details: await res.getModelListDetails(Firm),
         data,
       });
     }
@@ -81,35 +81,35 @@ module.exports = {
 
   update: async (req, res) => {
     /*
-            #swagger.tags = ["Categories"]
-            #swagger.summary = "Update Category"
+            #swagger.tags = ["Firms"]
+            #swagger.summary = "Update Firm"
             #swagger.parameters['body'] = {
                 in: 'body',
                 required: true,
                 schema: {
-                    "name": "Category 1"
+                    "name": "Firm 1"
                 }
             }
         */
 
-    const data = await Category.updateOne({ _id: req.params.id }, req.body, {
+    const data = await Firm.updateOne({ _id: req.params.id }, req.body, {
       runValidators: true,
     });
 
     res.status(200).send({
       error: false,
-      new: await Category.findOne({ _id: req.params.id }),
+      new: await Firm.findOne({ _id: req.params.id }),
       data,
     });
   },
 
   delete: async (req, res) => {
     /*
-            #swagger.tags = ["Categories"]
-            #swagger.summary = "Delete Category"
+            #swagger.tags = ["Firms"]
+            #swagger.summary = "Delete Firm"
         */
 
-    const data = await Category.deleteOne({ _id: req.params.id });
+    const data = await Firm.deleteOne({ _id: req.params.id });
 
     res.status(200).send({
       error: !data.deletedCount,
