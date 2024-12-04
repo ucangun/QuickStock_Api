c7d908049b69b873da60635203f4b88fa1a4cf1a
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-04 23:59:55 +0100

    "Fix typo in tokenHash.js: replaced `CHAR_COUNT` with `ENC_TYPE` in `encType` assignment"

--- src/helpers/tokenHash.js
+++ src/helpers/tokenHash.js
@@ -8,7 +8,7 @@ const { pbkdf2Sync } = require("crypto"),
   keyCode = process.env.SECRET_KEY,
   loopCount = process.env.LOOP_COUNT,
   charCount = process.env.CHAR_COUNT,
-  encType = process.env.CHAR_COUNT;
+  encType = process.env.ENC_TYPE;
 
 module.exports = function (password) {
   return pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString(
