8fad66c87c711d13f147d1dacc8f167bffc9e076
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-05 00:08:44 +0100

    "Updated tokenHash.js: changed type of loopCount and charCount to Number, added default value for encType"

--- src/helpers/tokenHash.js
+++ src/helpers/tokenHash.js
@@ -6,9 +6,9 @@
 
 const { pbkdf2Sync } = require("crypto"),
   keyCode = process.env.SECRET_KEY,
-  loopCount = process.env.LOOP_COUNT,
-  charCount = process.env.CHAR_COUNT,
-  encType = process.env.ENC_TYPE;
+  loopCount = Number(process.env.LOOP_COUNT),
+  charCount = Number(process.env.CHAR_COUNT),
+  encType = process.env.ENC_TYPE || "sha512";
 
 module.exports = function (password) {
   return pbkdf2Sync(password, keyCode, loopCount, charCount, encType).toString(
