7ed09125877734689a0f9cba83752c9a3f702c5b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-12-06 14:30:14 +0100

    "Added security middleware to Express app: helmet, rateLimit, mongoSanitize, xss, and hpp"

--- index.js
+++ index.js
@@ -5,6 +5,11 @@
 /* ------------------------------------------------- */
 
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const express = require("express");
 const app = express();
 
@@ -39,6 +44,26 @@ app.use(cors(corsOptions));
 
 // Accept JSON:
 app.use(express.json());
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/", limiter);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(hpp());
 
 // Check Authentication:
 app.use(require("./src/middlewares/authentication"));
